c2a2b18dce249718d55301790538aa20f59fb028
Jean <62931644+JeanWi@users.noreply.github.com>
2025-03-10 18:26:10 +0100

    run_model coded

--- run_model.py
+++ run_model.py
@@ -4,15 +4,27 @@ from pathlib import Path
 import pandas as pd
 
 data = {"Chile2030": {"p_heat": 126, "p_el": 100, "climate_data_sheet": "Chile_Data"},
+        "Chile2030_bl": {"p_heat": 126, "p_el": 100, "climate_data_sheet": "Baseline_Data"},
         "Chile2050": {"p_heat": 126, "p_el": 100, "climate_data_sheet": "Chile_Data"},
+        "Chile2050_bl": {"p_heat": 126, "p_el": 100, "climate_data_sheet": "Baseline_Data"},
         "Switzerland2030": {"p_heat": 27, "p_el": 70, "climate_data_sheet": "Switzerland_Data"},
-        "Switzerland2050": {"p_heat": 27, "p_el": 16.8, "climate_data_sheet": "Switzerland_Data"}}
+        "Switzerland2030_bl": {"p_heat": 27, "p_el": 70, "climate_data_sheet": "Baseline_Data"},
+        "Switzerland2050": {"p_heat": 27, "p_el": 16.8, "climate_data_sheet": "Switzerland_Data"},
+        "Switzerland2050_bl": {"p_heat": 27, "p_el": 16.8, "climate_data_sheet": "Baseline_Data"},
+        }
 
 input_data_path = Path("./caseStudies/dac")
 
-
 for case in data.keys():
-    print(case)
+
+    with open(input_data_path / "ConfigModel.json", "r") as json_file:
+        configuration = json.load(json_file)
+
+    configuration["optimization"]["emission_limit"]["value"] = -1000
+    # Save json template
+    with open(input_data_path / "ConfigModel.json", "w") as json_file:
+        json.dump(configuration, json_file, indent=4)
+
     climate_data = pd.read_excel(Path("./caseStudies/data/Template_DACInvest_filled_HEIDI.xlsx"), sheet_name=data[case]["climate_data_sheet"], skiprows=3)
     T = climate_data["temperature_2m (¬∞C)"][0:8760].to_list()
     RH = climate_data["relative_humidity_2m (%)"][0:8760].to_list()
@@ -28,7 +40,7 @@ for case in data.keys():
     cd.to_csv(input_data_path / "period1" / "node_data" / "n1" / "ClimateData.csv", sep=";")
 
     m = adopt.ModelHub()
-    m.read_data(input_data_path, start_period=0, end_period=1)
+    m.read_data(input_data_path)
+
+    m.data.model_config["reporting"]["case_name"]["value"] = case
     m.quick_solve()
-
-
